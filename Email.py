246cf60feca7d1516cb7a3d78d0e70a5e3abf735
Nicholas-Baldassini <46302707+Nicholas-Baldassini@users.noreply.github.com>
2021-08-17 11:16:53 -0400

    First version of bot works but needs improvment of determining which coin to hold

--- Email.py
+++ Email.py
@@ -1,27 +1,21 @@
 import smtplib, ssl
 
 port = 465  # For SSL
-# port = 587
-password = "BoomJohn_123"
-email = "DelTheFunkiestHomoSapien@gmail.com"
+password = "AgainWithTheMaestro123"
+email = "bottrader00002@gmail.com"
 smtp_server = "smtp.gmail.com"
 
 boom = "6479962879@fido.ca"
 john = "6473823566@msg.telus.com"
 Anthony = "6474662879@fido.ca"
+Selena = "6478844347@msg.telus.com"
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 
 message = """
-
-We are going to be fucking millionaires
-8====)
-
-I have the program pretty much set up but I need to install it
-on my desktop so we can run it 24/7, maybe tonight.
-
-8===============================)
+\t
+8-----
 """
 
 
@@ -29,8 +23,7 @@ def send_email(target: str, text: str):
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(email, password)
         server.sendmail(email, target, text)
-        # TODO: Send email here
 
 
-
-send_email(john, message)
+if __name__ == "__main__":
+    send_email(Selena, message)
--- WebScrap.py
+++ WebScrap.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import Email
 import time
 
 url = "https://www.tradingview.com/#signin"
 chart_url = "https://www.tradingview.com/chart"
-email = "DelTheFunkiestHomoSapien@gmail.com"
-password = "BoomJohn_123"
+password = "AgainWithTheMaestro123"
+email = "bottrader00002@gmail.com"
 
 PATH = "/Users/baldo/Applications/chromedriver"
 driver = webdriver.Chrome(PATH)
@@ -14,6 +14,7 @@ driver.get(url)
 driver.set_window_position(0, 0)
 driver.set_window_size(1300, 1200)
 
+# Signing into account
 driver.find_elements_by_class_name("tv-social__title")[2].click()
 driver.find_element_by_name("username").send_keys(email)
 driver.find_element_by_name("password").send_keys(password)
@@ -29,31 +30,139 @@ driver.find_element_by_class_name("tv-goto-chart-button").click()
 time.sleep(1)
 driver.switch_to.window(driver.window_handles[-1])
 time.sleep(2.5)
+
+# Enable indicator template for EMA's
 driver.find_element_by_id("header-toolbar-study-templates").click()
 time.sleep(1)
 driver.find_element_by_class_name("titleItem-2noQNU_F").click()
 driver.find_element_by_id("header-toolbar-intervals").click()
 
-# for e in driver.find_elements_by_class_name("item-2IihgTnv"):
-#     print(e.text)
+
+"""
+0 = 1 min
+1 = 3 min
+2 = 5 min
+3 = 15 min
+4 = 30 min
+5 = 45 min
+6 = 1 hour
+7 = 2 hour
+8 = 3 hour
+9 = 4 hour
+"""
+time_interval = 6
 time.sleep(0.5)
-driver.find_elements_by_class_name("item-2IihgTnv")[7].click()
+driver.find_elements_by_class_name("item-2IihgTnv")[time_interval].click()
+time.sleep(0.5)
+
+
+symbol_holder = {}
+symbol_sorted = []
+def update_watchlist():
+    global symbol_holder
+    global symbol_sorted
+    symbol_holder = {}
+    symbol_sorted = []
+    for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
+        stonk = e.text.split("\n")
+        # stonk = ["BTC", "643423.34", "-453.43", "2.98%"
+        if not stonk[-1][0].isnumeric():
+            stonk[-1] = stonk[-1].replace(chr(8722), '-')
+        symbol_sorted.append(float(stonk[-1][:-1]))
+        symbol_holder[float(stonk[-1][:-1])] = stonk[0]
+    symbol_sorted.sort(reverse=True)
+
+
+def find_possible_coins():
+    EMA_holder = {}
+    for e in driver.find_element_by_class_name("wrap-1a1_EyKG"):
+        try:
+            # Not all elements ae coins in the watchlist, so im cheating with
+            # this try/except :)
+            e.click()
+            time.sleep(0.5)
+            EMA_holder[e.text.split("\n")[0]] = None
+        except:
+            continue
+
+
+
 
 def get_emas():
     while True:
-        time.sleep(2)
-        price = driver.find_element_by_class_name("priceWrapper-3PT2D-PK").text
-        ema_3 = driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-1].text
-        ema_9 = driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-2].text
-        print(f"EMA3: {ema_3}")
-        print(f"EMA9: {ema_9}")
-        print(f"Price: {price}")
-        if float(ema_3) - float(ema_9) > 0:
-            print("BUY/HOLD")
-        else:
-            print("SELL/MONITOR")
-        print("=============================")
+        time.sleep(10)
+        update_watchlist()
+        for crypto in symbol_sorted:
+            for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
+                try:
+                    e.text.split("\n")[0]
+                except:
+                    continue
+                if e.text.split("\n")[0] == symbol_holder[crypto]:
+                    """
+                    At this point we have found a suitable crypto to analyze
+                    We now compute if this symbol is in a BUY/HOLD state or a 
+                    SELL/MONITOR state, we continue looping if SELL and we 
+                    continue refreshing and computing if on BUY
+                    """
+                    e.click()
+                    time.sleep(3)
+                    print("Analyzing ", {e.text.split('\n')[0]})
+                    bought = False
+                    ema_3 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-1].text)
+                    ema_9 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-2].text)
+                    symbol = e.text.split("\n")[0]
+                    price = e.text.split("\n")[1]
+                    t = time.localtime()
+                    l_time = time.strftime("%H:%M:%S", t)
 
+                    while ema_3 - ema_9 > 0.05:
+                        # Stay on this crypto
+                        symbol = e.text.split("\n")[0]
+                        price = e.text.split("\n")[1]
+                        t = time.localtime()
+                        l_time = time.strftime("%H:%M:%S", t)
+                        ema_3 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-1].text)
+                        ema_9 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-2].text)
+
+                        print("BUY/HOLDING")
+                        print(symbol, ": ", price)
+                        print("EMA DIFF: ", ema_3 - ema_9)
+                        print(f"Time: {l_time}")
+                        print("=================")
+                        if not bought:
+                            print("=======")
+                            print("SENT PURCHASE EMAIL")
+                            print("=======")
+                            m = f"""
+                            \t
+                            BUY:  {symbol}\n
+                            Price:  ${price}
+                            Time:  {l_time} EST
+                            EMA:  {round(ema_3 - ema_9, 6)}
+                            """
+                            Email.send_email(Email.boom, m)
+                            # Send Email to buy the symbol
+                            bought = True
+                        time.sleep(10)
+                    print("SELL/MONITOR")
+                    print("============")
+                    if bought:
+                        print("=======")
+                        print("SENT SELL EMAIL")
+                        print("=======")
+                        m = f"""
+                            \t
+                            SELL:  {symbol}\n
+                            Price:  ${price}
+                            Time:  {l_time} EST
+                            EMA:  {round(ema_3 - ema_9, 6)}
+                            """
+                        Email.send_email(Email.boom, m)
+                        # Send Email to sell the symbol and maybe some emojis for the celebration
+                        bought = False
+                    else:
+                        print(e.text.split("\n")[0], " is falling, don't buy")
 
 
 time.sleep(5)
