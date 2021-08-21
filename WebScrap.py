8521d499df96a4b8061ecb88a01af371c40b5a4f
Nicholas-Baldassini <46302707+Nicholas-Baldassini@users.noreply.github.com>
2021-08-20 20:14:48 -0400

    Down Memory lane

--- WebScrap.py
+++ WebScrap.py
@@ -10,6 +10,7 @@ email = "bottrader00002@gmail.com"
 PATH = "/Users/baldo/Applications/chromedriver"
 driver = webdriver.Chrome(PATH)
 driver.get(url)
+driver.set_page_load_timeout(5)
 
 driver.set_window_position(0, 0)
 driver.set_window_size(1300, 1200)
@@ -73,100 +74,217 @@ def update_watchlist():
     symbol_sorted.sort(reverse=True)
 
 
+def get_EMA():
+    ema_3 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-1].text)
+    ema_9 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-2].text)
+    return ema_3 - ema_9
+
+
 def find_possible_coins():
-    EMA_holder = {}
-    for e in driver.find_element_by_class_name("wrap-1a1_EyKG"):
+    """
+    This functions finds all the coins that are currently falling, we only want
+    to be looking at coins that are currently falling so then the moment that
+    they begin to rise we can buy instead of buying already rising coins and
+    risk purchasing at peaks, with this method we are guaranteed to sell at
+    higher prices than purchasing
+    """
+    print("Searching for negative EMA's")
+    negative_EMAS = []
+    for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
         try:
             # Not all elements ae coins in the watchlist, so im cheating with
             # this try/except :)
             e.click()
-            time.sleep(0.5)
-            EMA_holder[e.text.split("\n")[0]] = None
+            time.sleep(3)
+            EMA = get_EMA()
+            print(e.text.split("\n")[0], EMA)
+            print("Ema:" , get_EMA())
+            if EMA < 0.05:
+                # 0.5 SHOULD BE A RATIO TO EACH COIN NOT A HARD CODED 0.5
+                # ----------------------------------------------------
+                print("Negative: ", e.text.split("\n")[0])
+                negative_EMAS.append(e.text.split("\n")[0])
         except:
             continue
+    print("Finished searching for negative EMA's")
+    print("Negative EMA's: ", negative_EMAS)
+    print("===============================")
+    return negative_EMAS
 
 
+def look_for_changing_EMAS():
+    """
+    This function searchs through the negative EMA coins and checks if their
+    new EMA value has just become positive, if it does this implies a climb about
+    to take place and should imply a correct buy signal, this method ensures we
+    do we not buy during peaks and only buy during plateau's or dips.
+    """
+
+    negative = find_possible_coins()
+    print("Looking for changing EMA's")
+    time.sleep(5) # change to 30 sec
+    light_side = []
+    for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
+        try:
+            if e.text.split("\n")[0] in negative:
+                e.click()
+                time.sleep(3)
+                if get_EMA() > 0:
+                    # 0.5 SHOULD BE A RATIO TO EACH COIN NOT A HARD CODED 0.5
+                    # ----------------------------------------------------
+                    light_side.append(e.text.split("\n")[0])
+        except:
+            continue
+    print("Finished searching for changing EMA's")
+    print("Changed EMA's: ", light_side)
+    print("===============================")
+    return light_side
 
 
-def get_emas():
-    while True:
-        time.sleep(10)
-        update_watchlist()
-        for crypto in symbol_sorted:
+def hold(coin: str):
+    for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
+        if coin == e.text.split("\n")[0]:
+            print("Found :", coin)
+            e.click()
+            time.sleep(4)
+            symbol = e.text.split("\n")[0]
+            price = e.text.split("\n")[1]
+            t = time.localtime()
+            buy_time = time.strftime("%H:%M:%S", t)
+            print("=======")
+            print("SENT PURCHASE EMAIL")
+            print("=======")
+            m = f"""
+            \t
+            BUY:  {symbol}\n
+            Price:  ${price}
+            Time:  {buy_time} EST
+            EMA:  {round(get_EMA(), 6)}
+            """
+            Email.send_email(Email.boom, m)
+            time.sleep(10)
+            while get_EMA() > 0:
+                # Keep holding
+                print("Keep holding: ", symbol)
+                print("Price: ", e.text.split("\n")[1])
+                time.sleep(15)
+            print("SENT SELL EMAIL")
+            t = time.localtime()
+            sell_time = time.strftime("%H:%M:%S", t)
+            sell_price = "Not Found"
             for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
-                try:
-                    e.text.split("\n")[0]
-                except:
-                    continue
-                if e.text.split("\n")[0] == symbol_holder[crypto]:
-                    """
-                    At this point we have found a suitable crypto to analyze
-                    We now compute if this symbol is in a BUY/HOLD state or a 
-                    SELL/MONITOR state, we continue looping if SELL and we 
-                    continue refreshing and computing if on BUY
-                    """
-                    e.click()
-                    time.sleep(3)
-                    print("Analyzing ", {e.text.split('\n')[0]})
-                    bought = False
-                    ema_3 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-1].text)
-                    ema_9 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-2].text)
-                    symbol = e.text.split("\n")[0]
-                    price = e.text.split("\n")[1]
-                    t = time.localtime()
-                    l_time = time.strftime("%H:%M:%S", t)
+                if coin == e.text.split("\n")[0]:
+                    sell_price = e.text.split("\n")[1]
+            m = f"""
+            \t
+            Sell:  {symbol}\n
+            Price:  ${sell_price}
+            Time:  {sell_time} EST
+            EMA:  {round(get_EMA(), 6)}
+            """
+            Email.send_email(Email.boom, m)
 
-                    while ema_3 - ema_9 > 0.05:
-                        # Stay on this crypto
-                        symbol = e.text.split("\n")[0]
-                        price = e.text.split("\n")[1]
-                        t = time.localtime()
-                        l_time = time.strftime("%H:%M:%S", t)
-                        ema_3 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-1].text)
-                        ema_9 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-2].text)
-
-                        print("BUY/HOLDING")
-                        print(symbol, ": ", price)
-                        print("EMA DIFF: ", ema_3 - ema_9)
-                        print(f"Time: {l_time}")
-                        print("=================")
-                        if not bought:
-                            print("=======")
-                            print("SENT PURCHASE EMAIL")
-                            print("=======")
-                            m = f"""
-                            \t
-                            BUY:  {symbol}\n
-                            Price:  ${price}
-                            Time:  {l_time} EST
-                            EMA:  {round(ema_3 - ema_9, 6)}
-                            """
-                            Email.send_email(Email.boom, m)
-                            # Send Email to buy the symbol
-                            bought = True
-                        time.sleep(10)
-                    print("SELL/MONITOR")
-                    print("============")
-                    if bought:
-                        print("=======")
-                        print("SENT SELL EMAIL")
-                        print("=======")
-                        m = f"""
-                            \t
-                            SELL:  {symbol}\n
-                            Price:  ${price}
-                            Time:  {l_time} EST
-                            EMA:  {round(ema_3 - ema_9, 6)}
-                            """
-                        Email.send_email(Email.boom, m)
-                        # Send Email to sell the symbol and maybe some emojis for the celebration
-                        bought = False
-                    else:
-                        print(e.text.split("\n")[0], " is falling, don't buy")
+def find_coins_to_buy():
+    holding = []
+    # Currently only the first coint to be spotted to hold will be held, any
+    # other coins that are a good buy will be discarded
+    while True:
+        print("Looking for possible purchases....")
+        t = time.localtime()
+        local_time = time.strftime("%H:%M:%S", t)
+        print("Time: ", local_time)
+        coins = look_for_changing_EMAS()
+        if coins:
+            print("Found a possible coin: ", holding)
+            holding.extend(coins)
+            hold(holding[0])
+            holding.clear()
+        else:
+            print("No purchases found.")
 
 
-time.sleep(5)
-get_emas()
+
+time.sleep(2)
+find_coins_to_buy()
+# def get_emas():
+#     while True:
+#         time.sleep(10)
+#         update_watchlist()
+#         for crypto in symbol_sorted:
+#             for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
+#                 try:
+#                     e.text.split("\n")[0]
+#                 except:
+#                     continue
+#                 if e.text.split("\n")[0] == symbol_holder[crypto]:
+#                     """
+#                     At this point we have found a suitable crypto to analyze
+#                     We now compute if this symbol is in a BUY/HOLD state or a
+#                     SELL/MONITOR state, we continue looping if SELL and we
+#                     continue refreshing and computing if on BUY
+#                     """
+#                     e.click()
+#                     time.sleep(3)
+#                     print("Analyzing ", {e.text.split('\n')[0]})
+#                     bought = False
+#                     ema_3 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-1].text)
+#                     ema_9 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-2].text)
+#                     symbol = e.text.split("\n")[0]
+#                     price = e.text.split("\n")[1]
+#                     t = time.localtime()
+#                     l_time = time.strftime("%H:%M:%S", t)
+#
+#                     while ema_3 - ema_9 > 0.05:
+#                         # Stay on this crypto
+#                         symbol = e.text.split("\n")[0]
+#                         price = e.text.split("\n")[1]
+#                         t = time.localtime()
+#                         l_time = time.strftime("%H:%M:%S", t)
+#                         ema_3 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-1].text)
+#                         ema_9 = float(driver.find_elements_by_class_name("valueValue-2KhwsEwE")[-2].text)
+#
+#                         print("BUY/HOLDING")
+#                         print(symbol, ": ", price)
+#                         print("EMA DIFF: ", ema_3 - ema_9)
+#                         print(f"Time: {l_time}")
+#                         print("=================")
+#                         if not bought:
+#                             print("=======")
+#                             print("SENT PURCHASE EMAIL")
+#                             print("=======")
+#                             m = f"""
+#                             \t
+#                             BUY:  {symbol}\n
+#                             Price:  ${price}
+#                             Time:  {l_time} EST
+#                             EMA:  {round(ema_3 - ema_9, 6)}
+#                             """
+#                             Email.send_email(Email.boom, m)
+#                             # Send Email to buy the symbol
+#                             bought = True
+#                         time.sleep(10)
+#                     print("SELL/MONITOR")
+#                     print("============")
+#                     if bought:
+#                         print("=======")
+#                         print("SENT SELL EMAIL")
+#                         print("=======")
+#                         m = f"""
+#                             \t
+#                             SELL:  {symbol}\n
+#                             Price:  ${price}
+#                             Time:  {l_time} EST
+#                             EMA:  {round(ema_3 - ema_9, 6)}
+#                             """
+#                         Email.send_email(Email.boom, m)
+#                         # Send Email to sell the symbol and maybe some emojis for the celebration
+#                         bought = False
+#                     else:
+#                         print(e.text.split("\n")[0], " is falling, don't buy")
+#
+#
+# time.sleep(5)
+# get_emas()
 
 
 time.sleep(100)
