b0d65fbc781664dabaf76a8e24770f5abecf0a52
Nicholas-Baldassini <nichollas.baldassini@gmail.com>
2021-09-04 12:45:12 -0400

    V3.2 version seems to increased in speed but makes far too many incorrect calls due to a technical bug

--- WebScrap.py
+++ WebScrap.py
@@ -3,11 +3,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import InformationExtract as IE
+import Email
+import threading
 
 
 url = "https://www.tradingview.com/#signin"
 password = IE.Email_Password[0]
 email = IE.Email_Sender[0]
+email_list = [Email.boom]
+start_time = time.time()
+stop_loss = 0.003
 
 MAC_PATH = IE.Driver_Path[1]
 WIN_PATH = IE.Driver_Path[0]
@@ -26,20 +31,6 @@ driver.find_element_by_name("password").send_keys(password)
 driver.find_element_by_class_name("tv-button__loader").click()
 time.sleep(2.5)
 
-
-# time.sleep(1.5)
-# driver.find_element_by_class_name("tv-goto-chart-button").click()
-# time.sleep(1)
-# driver.switch_to.window(driver.window_handles[-1])
-# time.sleep(2.5)
-
-# # Enable indicator template for EMA's
-# driver.find_element_by_id("header-toolbar-study-templates").click()
-# time.sleep(1)
-# driver.find_element_by_class_name("titleItem-2noQNU_F").click()
-# driver.find_element_by_id("header-toolbar-intervals").click()
-
-
 """
 0 = 1 min
 1 = 3 min
@@ -52,12 +43,177 @@ time.sleep(2.5)
 8 = 3 hour
 9 = 4 hour
 """
-# index_to_seconds = [60, 180, 5 * 60, 15 * 60, 30 * 60, 45 * 60, 60 * 60, 120 * 60, 180 * 60]
-# # index from the above comment corresponds the amount in seconds ^^^
-# time_interval = 3
-# time.sleep(0.5)
-# driver.find_elements_by_class_name("item-2IihgTnv")[time_interval].click()
-# time.sleep(0.5)
+# index from the above comment corresponds the amount in seconds ^^^
+time_interval = 2
+index_to_seconds = [60, 180, 5 * 60, 15 * 60, 30 * 60, 45 * 60, 60 * 60, 120 * 60, 180 * 60]
+coins = {}
+
+
+class coin:
+    def __init__(self, symbol: str, buy_n=3, sell_n=4):
+        """
+        symbol: symbol of the coin
+        buy_n: MACD indicator buy variable
+        sell_n: MACD indicator sell variable
+        """
+        self.symbol = symbol
+        self.price = 0
+        self.EMA = None
+        self.macd = []
+        self.hold = False
+        self.buy_n = buy_n
+        self.sell_n = sell_n
+        self.curr_time = time.time()
+        self.stop_loss = None
+
+    def __str__(self):
+        """
+        Cleaner output of coin
+        """
+        return f"{self.symbol}: {self.price}\n EMA: {self.EMA}|| MACD: {self.macd}\nState: {self.hold}\n=========="
+
+    def __bool__(self):
+        """
+        A coin is truthy if it is in a hold state
+        """
+        return self.hold
+
+    def update_other(self, price: float, ema: float):
+        if price > self.price:
+            # if new price is greate than the old price, increase the stop_loss
+            self.stop_loss = (1 - stop_loss) * price
+        self.price = price
+        self.EMA = ema
+
+    def update_macd(self, val: float) -> None:
+        """
+        Update the macd indicator for the coin, keep self.n macd values
+        """
+        if time.time() - self.curr_time > index_to_seconds[time_interval]:
+            # Only update the macd each interval
+            if len(self.macd) < max(self.buy_n, self.sell_n):
+                self.macd.append(val)
+            else:
+                self.macd.pop(0)
+                self.macd.append(val)
+            self.curr_time = time.time()
+        #assert len(self.macd) == max(self.buy_n, self.sell_n)
+
+    def test_macd(self) -> bool:
+        """
+        Return the state of the macd
+
+        If all val in macd are increasing: BUY
+
+        Hold until all values in macd are decreasing
+        """
+        if self.hold:
+            if self.macd[-1] < 0:
+                # If we are holding and get a negative MACD, sell instantly
+                print(f"NEGATIVE MACD SELL: {self.symbol}")
+                return False
+            if all(self.macd[i] > self.macd[i + 1] for i in range(len(self.macd[-self.sell_n:]))):
+                # If we are holding and get all decreasing MACD in a row, sell instantly
+                print(f"DECREASING MACD SELL: {self.symbol}")
+                return False
+            # If no sell signals occur, return True to keep buy/holding
+            return True
+        else:
+            # If currently not holding and n increasing MACD in a row indicates a buy signal
+            if all(self.macd[i] < self.macd[i + 1] for i in range(len(self.macd[-self.buy_n:]))):
+                if all(self.macd[i] > 0 for i in range(len(self.macd[-self.buy_n + 1:]))):
+                    # if the the macds are positive and increasing
+                    print(f"INCREASING MACD: {self.symbol}")
+                    return True
+            return False
+
+    def test_state(self):
+        """
+        This is where we can make our buy and sell conditions, constantly test the state of the coin to see
+        if it is in a buy/hold state or a sell/monitor state
+        """
+
+        if len(self.macd) == max(self.buy_n, self.sell_n):
+            # Only start testing once we have enough data
+            if self.EMA > 0 and self.test_macd() and self.price > self.stop_loss:
+                # Will be true if macd does not give a sell signal
+                # This indicates a buy/hold signal
+                if not self.hold:
+                    # Coin just went from a sell/monitor to a buy/hold state so this indicate a buy signal
+                    m = f"\tBUY SIGNAL: {self.symbol}\nTime: {get_time()}\nPrice: {get_price()}\nMACD: {self.macd}\nEMA: {self.EMA}"
+                    print(m)
+                    Email.send_email(email_list, m)
+                self.hold = True
+            else:
+                # No buy/hold signal detected
+                if self.hold:
+                    # Coin just went from a buy/hold state to a sell/monitor state, this indicates a sell signal
+                    m = f"\tSELL SIGNAL: {self.symbol}\nTime: {get_time()}\nPrice: {get_price()}\nMACD: {self.macd}\nEMA: {self.EMA}"
+                    print(m)
+                    Email.send_email(email_list, m)
+                self.hold = False
+
+
+def boot_up():
+    global coins
+    action = ActionChains(driver)
+    chart = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div[2]/div[2]/div[2]/div/ul/li[1]")
+
+
+    action.key_down(Keys.CONTROL)
+    action.perform()
+
+    for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
+        chart.click()
+        time.sleep(1)
+
+    action.key_up(Keys.CONTROL)
+    action.perform()
+
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+    if driver.title == "429 Too Many Requests":
+        driver.refresh()
+    time.sleep(2)
+    coin_symbols = [e.text.split("\n")[0] for e in driver.find_elements_by_class_name("wrap-1a1_EyKG")]
+    assert coin_symbols
+    for ind, tab in enumerate(driver.window_handles):
+        driver.switch_to.window(tab)
+        time.sleep(0.5)
+        if driver.title == "429 Too Many Requests":
+            driver.refresh()
+            time.sleep(2)
+        click_on_watchlist(coin_symbols[ind])
+        coins[coin_symbols[ind]] = coin(coin_symbols[ind])
+    # Enable indicator template for EMA's
+    driver.find_element_by_id("header-toolbar-study-templates").click()
+    time.sleep(1)
+    driver.find_element_by_class_name("titleItem-2noQNU_F").click()
+    driver.find_element_by_id("header-toolbar-intervals").click()
+    time.sleep(0.5)
+    driver.find_elements_by_class_name("item-2IihgTnv")[time_interval].click()
+    time.sleep(0.5)
+    time.sleep(5)
+
+
+def analyzing():
+    # This is the main loop, anything caught in this loop should be multithreading
+    while True:
+        for tab in driver.window_handles:
+            driver.switch_to.window(tab)
+            tab_symbol = get_symbol()
+            c = coins[tab_symbol]
+            c.update_other(get_price(), get_EMA())
+            # c.price = get_price()
+            # c.EMA = get_EMA()
+            c.update_macd(get_MACD())
+            c.test_state()
+
+            # Todo we do not want to be updating the macd every second but rather every interval
+            # So change this garbage code, udpdate macd and ema every interval
+
+        for c in coins.values(): print(c.symbol, c.macd)
+        print("==================")
 
 
 def get_time() -> str:
@@ -96,22 +252,23 @@ def get_MACD():
     return float(macd.text.replace(chr(8722), '-'))
 
 
+def get_price():
+    return float(driver.find_element_by_class_name("highlight-2GhssDiZ").text)
+
+
 def click_on_watchlist(coin: str) -> bool:
     """
     Click on the desired coin on the watchlist to bring up its chart
     """
     for e in driver.find_elements_by_class_name("wrap-1a1_EyKG"):
         if e.text.split("\n")[0] == coin:
+            driver.execute_script("arguments[0].scrollIntoView();", e)
             e.click()
             print("Clicked on ", coin)
             time.sleep(2)
             return True
     print(f"Could not find {coin} to click in watchlist")
     return False
-
-
-def get_price():
-    return float(driver.find_element_by_class_name("highlight-2GhssDiZ").text)
 
 
 def find_possible_coins() -> list:
--- main.py
+++ main.py
@@ -6,42 +6,44 @@ from random import randint
 
 def start():
     import WebScrap as W
+    W.boot_up()
+    W.analyzing()
 
-    while True:
-        print("TIME: ", W.get_time())
-        possible_coins = W.look_for_changing_EMAS()
-
-        if possible_coins:
-            symbol = possible_coins[randint(0, len(possible_coins) - 1)]
-            assert W.click_on_watchlist(symbol)
-            # Currently only the first coin to be spotted to hold will be held, any
-            # other coins that are a good buy will be discarded
-            print(f"Analyzing:  {symbol}", "Starting MACD test...", end="\n")
-
-            # Email.send_email(Email.boom, f"\tCurrently analyzing {symbol} MACD at : {W.get_time()}")
-
-            if W.MACD_test_buy():
-                # Start while loop with W.MACD_HOLD_SELL() to keep holding until
-                # MACD decreases or stop loss
-                print(f"Purchase {symbol} and hold!!!!")
-                m = f"""\t
-                Buy: {symbol}
-                \tprice: {W.get_price()}
-                Time: {W.get_time()}
-                """
-                print(f"Price: {W.get_price()}", f"Time: {W.get_time()}", sep="\n")
-                Email.send_email([Email.boom], m)
-                W.MACD_hold_and_sell()
-                print("==================", f"SELL ALERT:  {symbol}", "==================", sep="\n")
-                m = f"""\t
-                Sell: {symbol}
-                \tprice: {W.get_price()}
-                Time: {W.get_time()}
-                """
-                Email.send_email([Email.boom], m)
-        else:
-            print("No possible purchases found, restarting...")
-        time.sleep(10)
+    # while True:
+    #     print("TIME: ", W.get_time())
+    #     possible_coins = W.look_for_changing_EMAS()
+    #
+    #     if possible_coins:
+    #         symbol = possible_coins[randint(0, len(possible_coins) - 1)]
+    #         assert W.click_on_watchlist(symbol)
+    #         # Currently only the first coin to be spotted to hold will be held, any
+    #         # other coins that are a good buy will be discarded
+    #         print(f"Analyzing:  {symbol}", "Starting MACD test...", end="\n")
+    #
+    #         # Email.send_email(Email.boom, f"\tCurrently analyzing {symbol} MACD at : {W.get_time()}")
+    #
+    #         if W.MACD_test_buy():
+    #             # Start while loop with W.MACD_HOLD_SELL() to keep holding until
+    #             # MACD decreases or stop loss
+    #             print(f"Purchase {symbol} and hold!!!!")
+    #             m = f"""\t
+    #             Buy: {symbol}
+    #             \tprice: {W.get_price()}
+    #             Time: {W.get_time()}
+    #             """
+    #             print(f"Price: {W.get_price()}", f"Time: {W.get_time()}", sep="\n")
+    #             Email.send_email([Email.boom], m)
+    #             W.MACD_hold_and_sell()
+    #             print("==================", f"SELL ALERT:  {symbol}", "==================", sep="\n")
+    #             m = f"""\t
+    #             Sell: {symbol}
+    #             \tprice: {W.get_price()}
+    #             Time: {W.get_time()}
+    #             """
+    #             Email.send_email([Email.boom], m)
+    #     else:
+    #         print("No possible purchases found, restarting...")
+    #     time.sleep(10)
 
 
 if __name__ == "__main__":
